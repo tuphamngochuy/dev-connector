7aac72b4d466b5af6358f09c3192bef7f592e203
huytn0601 <huytn0601@gmail.com>
2022-09-28 23:08:06 +0700

    connect mongoDB

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
