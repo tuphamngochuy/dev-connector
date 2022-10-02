f1614ce09b6932ead0de45faa6ae8cbb175e0fb1
huytn0601 <huytn0601@gmail.com>
2022-10-02 14:51:23 +0700

    complete posts model and build up routes

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -9,17 +9,48 @@ const router = express.Router();
  * @desc    Create new post
  * @access  Private
  */
+router.post('/', authentication);
+
+/**
+ * @route   GET api/posts/me
+ * @desc    Get all user's posts
+ * @access  Private
+ */
 
 /**
  * @route   GET api/posts/post?post_id
- * @desc    Get post by post_id
+ * @desc    Get post by post's ID
  * @access  Public
  */
 
 /**
- * @route   GET api/posts/posts?user_id
- * @desc    Get post by post_id
- * @access  Public
+ * @route   DELETE api/posts/post?post_id
+ * @desc    Delete post by post's ID
+ * @access  Private
+ */
+
+/**
+ * @route   PUT api/posts/like?post_id
+ * @desc    Like a post
+ * @access  Private
+ */
+
+/**
+ * @route   PUT api/posts/unlike?post_id
+ * @desc    Unlike a post
+ * @access  Private
+ */
+
+/**
+ * @route   PUT api/posts/comment?post_id
+ * @desc    Comment is a post
+ * @access  Private
+ */
+
+/**
+ * @route   PUT api/posts/uncomment?post_id
+ * @desc    Uncomment is a post
+ * @access  Private
  */
 
 module.exports = router;
